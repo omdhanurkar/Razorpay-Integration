bdf0403e970cd01e6ced00c6ebbad8e43f09fea0
Omprakash <omdhanurkar444@gmail.com>
2023-02-26 23:12:49 +0530

    PaymentSuccess.js

--- frontend/src/Components/PaymentSuccess.js
+++ frontend/src/Components/PaymentSuccess.js
@@ -5,7 +5,7 @@ import { useSearchParams } from "react-router-dom"
 
 const PaymentSuccess = () => {
 
-    swal("Thank you!", "Your Order is Done!", "success");
+    swal("Thank you!", "Your Payment is Done!", "success");
 
     const seachQuery = useSearchParams()[0]
 
@@ -16,7 +16,7 @@ const PaymentSuccess = () => {
         <Box>
             <VStack h="100vh" justifyContent={"center"}>
 
-                <Heading textTransform={"uppercase"}> Order Details</Heading>
+                <Heading textTransform={"uppercase"}>Payment Details</Heading>
 
                 <Text>
                     Reference No.{referenceNum}
