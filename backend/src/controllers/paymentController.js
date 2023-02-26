9a4d005175803b9e7a58a3c6f30255fff870940a
Omprakash <omdhanurkar444@gmail.com>
2023-02-26 23:01:50 +0530

    paymentController

--- backend/src/controllers/paymentController.js
+++ backend/src/controllers/paymentController.js
@@ -5,6 +5,7 @@ const Payment = require("../models/paymentModel")
 
 
 //===========================configure razorpay=================================================
+
 const instance = new Razorpay({
     key_id: process.env.RAZORPAY_API_KEY,
     key_secret: process.env.RAZORPAY_APT_SECRET,
@@ -35,19 +36,15 @@ const paymentVerification = async (req, res) => {
         .update(body.toString())
         .digest("hex");
 
-    
-
     const isAuthentic = expectedSignature === razorpay_signature;
 
     if (isAuthentic) {
         // Database comes here
-
         await Payment.create({
             razorpay_order_id,
             razorpay_payment_id,
             razorpay_signature,
         });
-
         res.redirect(
             `http://localhost:3000/PaymentSuccess?reference=${razorpay_payment_id}`
         );
