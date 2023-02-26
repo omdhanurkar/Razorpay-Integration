e4210210ca327941f349dceee912873b15d9d78f
Omprakash <omdhanurkar444@gmail.com>
2023-02-26 23:11:07 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import PaymentSuccess from "./Components/PaymentSuccess"
 
-
-
 function App() {
   return (
     <BrowserRouter>
